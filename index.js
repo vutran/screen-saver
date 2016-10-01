220f784a01b2ee5b727c8e3254afea6f9a9303cc
Rasberry <rasberry@hotmail.com>
2016-10-01 00:16:56 -0600

    Invoking the screensaver using a verbose but native-only command

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = () => {
   if (process.platform === 'darwin') {
     return execFile('open', ['/System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app']);
   } else if (process.platform === 'win32') {
-    return execFile('nircmd.exe', ['screensaver']);
+    return execFile('cmd.exe', ['/c', 'for', '/f', 'tokens=3* delims= ', '%a', 'in', '(\'reg', 'query', 'HKCU\\Control Panel\\Desktop', '/v', 'SCRNSAVE.EXE\')', 'do', '%a', '/run']);
   } else if (process.platform === 'linux') {
     return exec('xdg-screensaver', ['activate']);
   }
